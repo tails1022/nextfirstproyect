03b17f49b989a6e4d378bb6b333181cf9f2c8adf
tails1022 <juanpablolopezdiaz1022@gmail.com>
2023-05-24 20:24:34 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Next Simple Portfolio
+![](./screenshot.png)
+
+# Tools
+
+- Nextjs
+- Bootswatch 4
+
+## Resources
+
+- [NProgress change color](https://github.com/rstacruz/nprogress/issues/187)
+- [Nextjs 100%](https://gist.github.com/dmurawsky/d45f068097d181c733a53687edce1919)
+"# nextfirstproyect" 
---
+++ components/Navbar.js
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+ return (
+  <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+   <div className='container'>
+    <Link legacyBehavior href='/'>
+     <a className='navbar-brand'>Simple Portfolio</a>
+    </Link>
+    <button
+     className='navbar-toggler'
+     type='button'
+     data-bs-toggle='collapse'
+     data-bs-target='#navbarNav'
+     aria-controls='navbarNav'
+     aria-expanded='false'
+     aria-label='Toggle navigation'
+    >
+     <span className='navbar-toggler-icon'></span>
+    </button>
+    <div className='collapse navbar-collapse' id='navbarNav'>
+     <ul className='navbar-nav ml-auto'>
+      <li className='nav-item'>
+       <Link legacyBehavior href='/blog'>
+        <a className='nav-link active' aria-current='page'>
+         Blog
+        </a>
+       </Link>
+      </li>
+      <li className='nav-item'>
+       <Link legacyBehavior href='/github'>
+        <a className='nav-link'>Github</a>
+       </Link>
+      </li>
+     </ul>
+    </div>
+   </div>
+  </nav>
+ )
+}
+
+export default Navbar
